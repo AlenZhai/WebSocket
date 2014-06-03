ae005603a772b797e904c33c342f234dc5e5d074
AlenZhai <zhaizhisheng@sina.com>
2014-06-03 11:37:08 +0800

    remove read sleep

--- WebSocket/webapp/src/main/java/com/websocket/handler/DataHandle.java
+++ WebSocket/webapp/src/main/java/com/websocket/handler/DataHandle.java
@@ -1,44 +1,51 @@
 package com.websocket.handler;
 import com.philips.transport.tcp.emis_server.pack_type.DataPackFormat;
+import com.philips.transport.tcp.emis_server.utils.ECacheManager;
 import com.websocket.servlet.SocketServlet;
+import com.websocket.util.ConnectionsManager;
+
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class DataHandle extends Thread {
 	private final Logger logger = LoggerFactory.getLogger(DataHandle.class);
 	private boolean stop = false;
 	private Set<SocketServlet> connections;
-	Map<String, Queue<DataPackFormat>> queueMap;
-
-	public DataHandle(Set<SocketServlet> connections,
-			Map<String, Queue<DataPackFormat>> queueMap) {
-		this.logger.debug("init dataHandle ...");
-		this.connections = connections;
-
-		this.queueMap = queueMap;
+	private ConcurrentMap<String,Queue<DataPackFormat>> queueMap;
+	public DataHandle() {
+		this.logger.debug("init dataHandle ...");	
+		connections=ConnectionsManager.getNewInstance().getConnections();
+		queueMap=ECacheManager.getNewInstance().getQueueMap();
 	}
 
 	public void run() {
-		DataPackFormat data;
-		while (!this.stop) {
-			for (String ip : this.queueMap.keySet()) {
-				data = (DataPackFormat) ((Queue) this.queueMap.get(ip)).poll();
-				if (data != null) {
-					data.setClinetIp(ip);
-					for (SocketServlet client : this.connections) {
+		
+		while (!this.stop) {	
+			for(String key:queueMap.keySet())
+			{
+				Queue<DataPackFormat> queue=queueMap.get(key);
+				DataPackFormat data = queue.poll();
+				/*try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}*/
+				if (data != null) {					
+					for (SocketServlet client :connections ) {
+						
 						synchronized (client) {
 							client.broadcast(data);
 						}
 					}
 
 				}
-
 			}
-
 		}
-
 		this.logger.debug("stop dataHandle data");
 	}
 
